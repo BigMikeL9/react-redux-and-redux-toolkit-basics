95386393951936f9c025b6c3e3cb23af152556fa
MikeL Kamel <mikel1832@yahoo.com>
2022-07-27 14:39:32 -0400

    Using Redux Toolkit configureStore function to merge multiple reducer function and use it for our store

--- src/store/counterStore.js
+++ src/store/counterStore.js
@@ -1,8 +1,50 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 // REDUX STORE
 
 const initialState = { counter: 0, showCounter: true };
+
+// -----------------------------------------------------------------------------------------
+// 🟢 Redux Toolkit
+
+const counterSlice = createSlice({
+  name: " counter",
+  initialState, // --> 📝 Remember ES6+ syntax that is same as writing 'initialState: initialState,'
+  reducers: {
+    increment(state, action) {
+      // --> 👆👆👆 📝 Remember ES6+ object methods syntax that is the same as writing 'increment: function() {....},'
+      state.counter++;
+      // --> 👆👆👆 ⭐ even tho it seems we are DIRECTLY mutating state property, 'IMMER' package updates the new state in an IMMUTABLE manner behind the scenes by returning a new state object that overrides the previous one. See How STANDARD Redux without Redux Toolkit is setup below and see notes.
+    },
+    decrement(state, action) {
+      state.counter--;
+    },
+    increase_By(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
+
+console.log(counterSlice);
+
+const counterStore = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+  },
+});
+
+console.log(counterStore);
+
+export default counterStore;
+/*
+// -----------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------
+/🔴 STANDARD Redux  WITHOUT  Redux Toolkit
+
+import { createStore } from "redux";
 
 // 1. Create 'Reducer function'
 const counterReducer = (state = initialState, action) => {
@@ -44,5 +86,6 @@ const counterReducer = (state = initialState, action) => {
 const counterStore = createStore(counterReducer);
 
 // 3. Provide the Redux store to the React App. ie:  Connect the React app to the Redux store, so that components in this App can dispatch and listen to changes in the store.
-
-export default counterStore;
+// export default counterStore;
+// -----------------------------------------------------------------------------------------
+*/
